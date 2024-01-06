8c7f264642a2137a57c1de0d864e8d763d9aa731
ishi12121 <isht1402@gmail.com>
2024-01-06 19:21:13 +0530

    frontend backend connected

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -8,9 +9,20 @@ const app = express();
 app.use(cors({
     origin:'http://localhost:3000',
 }));
+
+
 app.use(express.json());
 
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
 app.use(planetsRouter);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..\\public\\index.html'));
+});
+
+
 module.exports = app;
 
 
